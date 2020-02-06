980f8224f128a8f07d19d3bb4df11148e003f01a
Chloe Lee <chllee29@uw.edu>
2020-02-06 15:21:33 -0800

    week 5 book exercises

--- chapter-14-exercises/exercise-1/exercise.R
+++ chapter-14-exercises/exercise-1/exercise.R
@@ -6,6 +6,7 @@
 
 # Create a variable base_uri that stores the base URI (as a string) for the 
 # Github API (https://api.github.com)
+base_uri <- "https://api.github.com"
 
 
 # Under the "Repositories" category of the API documentation, find the endpoint 
@@ -13,12 +14,14 @@
 # `org_resource` that stores the endpoint for the `programming-for-data-science`
 # organization repos (this is the _path_ to the resource of interest).
 
+org_resource <- 
+
 
 # Send a GET request to this endpoint (the `base_uri` followed by the 
 # `org_resource` path). Print the response to show that your request worked. 
 # (The listed URI will also allow you to inspect the JSON in the browser easily).
 
-
+GET(paste0(base_uri, org_resource))
 # Extract the content of the response using the `content()` function, saving it
 # in a variable.
 
